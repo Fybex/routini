254ace9091f6872adfbaaf2796240c67d78946f2
Andrew <46112160+Fybex@users.noreply.github.com>
2021-10-09 09:49:28 +0300

    added the ability to change files

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,13 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Box } from '@mui/system';
 
-import Page from '../page/page';
+
 import Sidebar from '../sidebar/sidebar';
+import Editor from '../editor/editor';
 
 const App = () => {
+    const [folders, setFolder] = useState([{ id: 2, title: 'Школа', text: '<p>Hello World! 🌎️</p>' }, { id: 3, title: 'Література', text: '<h3>Literature!</h3>' }]);
+    const [activeFile, setActiveFile] = useState(1);
+    
+
+    const getActiveFile = () => {
+        // console.log(folders.find(folder => folder.id === activeFolder));
+        if(folders.find(folder => folder.id === activeFile)) {
+            return folders.find(folder => folder.id === activeFile)
+        }   else {
+            return false;
+        }
+    }
+
+    const onUpdateNote = (updatedNote) => {
+        const updatedFoldersArr = folders.map(folder => {
+            if(folder.id === updatedNote.id) {
+                return updatedNote;
+            }
+            return folder;
+        });
+        setFolder(updatedFoldersArr);
+    }
+
     return (
         <>
-            <Sidebar/>
-            <Page/>
+            <Box sx={{ display: 'flex' }}>
+                <Sidebar folders={folders} setActiveFile={setActiveFile} />
+                <Editor activeFile={getActiveFile()} onUpdateNote={onUpdateNote} />
+            </Box>
+
         </>
     )
 }
--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,101 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { Drawer, Box, Toolbar, CssBaseline, List, ListItem, ListItemText, ListItemIcon, ListItemButton, Collapse, Divider, AppBar, Typography, Button } from '@mui/material';
-import NotesIcon from '@mui/icons-material/Notes';
-import NoteIcon from '@mui/icons-material/Note';
-import ExpandLess from '@mui/icons-material/ExpandLess';
-import ExpandMore from '@mui/icons-material/ExpandMore';
-import StarBorder from '@mui/icons-material/StarBorder';
+import { Drawer, Box, CssBaseline, Typography, Button } from '@mui/material';
+import TreeView from '@mui/lab/TreeView';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import TreeItem from '@mui/lab/TreeItem';
 
-import './sidebar.css';
 
 const drawerWidth = 240;
 
-const Sidebar = (props) => {
-    const { window } = props;
-    const [mobileOpen, setMobileOpen] = React.useState(false);
-    const [open, setOpen] = React.useState(true);
+const Sidebar = ({ folders, setActiveFile }) => {
 
-    const handleDrawerToggle = () => {
-        setMobileOpen(!mobileOpen);
-    };
+    const createFolder = () => {
 
-    const handleClick = () => {
-        setOpen(!open);
-    };
+    }
 
-    const drawer = (
-        <div>
-            <Typography sx={{ fontWeight: 'bold', pl: 2, pt: 5 }} variant='h5'>routini</Typography>
-            <List sx={{my: 5}}>
-                <ListItem button key='id-notes'>
-                    <ListItemIcon>
-                        <NotesIcon/>
-                    </ListItemIcon>
-                    <ListItemText primary='Всі задачі'/>
-                </ListItem>
-            </List>
-            <ListItemButton onClick={handleClick}>
-                <ListItemIcon>
-                <NoteIcon />
-                </ListItemIcon>
-                <ListItemText primary="Папки" />
-                {open ? <ExpandLess /> : <ExpandMore />}
-            </ListItemButton>
-            <Collapse in={open} timeout="auto" unmountOnExit>
-                <List component="div" disablePadding>
-                <ListItemButton sx={{ pl: 4 }}>
-                    <ListItemIcon>
-                        <StarBorder />
-                    </ListItemIcon>
-                    <ListItemText primary="Школа" />
-                </ListItemButton>
-                <ListItemButton sx={{ pl: 4 }}>
-                    <ListItemIcon>
-                        <StarBorder />
-                    </ListItemIcon>
-                    <ListItemText primary="Література" />
-                </ListItemButton>
-                </List>
-            </Collapse>
-            <Button variant='outlined' sx={{ml: 2, mt: 3}}>Додати папку</Button>
-        </div>
-    );
-
-    const container = window !== undefined ? () => window().document.body : undefined;
+    const elements = folders.map(folder => {
+        const { id, title } = folder;
+        return (
+            <TreeItem sx={{ p: 0.5 }} nodeId={`${id}`} label={title} onClick={() => setActiveFile(id)} />
+        )
+    })
 
     return (
-        <Box sx={{ display: 'flex' }}>
+        <>
             <CssBaseline />
-            <AppBar
-                position="fixed"
-                sx={{
-                    width: { sm: `calc(100% - ${drawerWidth}px)` },
-                    ml: { sm: `${drawerWidth}px` },
-                }}
-            >
-            </AppBar>
             <Box
                 component="nav"
                 sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
                 aria-label="mailbox folders"
             >
-                {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-                <Drawer
-                    container={container}
-                    variant="temporary"
-                    open={mobileOpen}
-                    onClose={handleDrawerToggle}
-                    ModalProps={{
-                        keepMounted: true, // Better open performance on mobile.
-                    }}
-                    sx={{
-                        display: { xs: 'block', sm: 'none' },
-                        '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-                    }}
-                >
-                    {drawer}
-                </Drawer>
                 <Drawer
                     variant="permanent"
                     sx={{
@@ -104,11 +38,24 @@ const Sidebar = (props) => {
                     }}
                     open
                 >
-                    {drawer}
+                    <Box sx={{ ml: 2, mt: 5 }}>
+                        <Typography sx={{ fontWeight: 'bold' }} variant='h5'>routini</Typography>
+                        <TreeView
+                            aria-label="multi-select"
+                            defaultCollapseIcon={<ExpandMoreIcon />}
+                            defaultExpandIcon={<ChevronRightIcon />}
+                            multiSelect
+                            sx={{ overflowY: 'auto', my: 3 }}
+                        >
+                            <TreeItem nodeId="1" label="Замітки">
+                                {elements}
+                            </TreeItem>
+                        </TreeView>
+                        <Button variant='outlined' onClick={createFolder} >Додати папку</Button>
+                    </Box>
                 </Drawer>
             </Box>
-
-        </Box>
+        </>
     );
 }
 
