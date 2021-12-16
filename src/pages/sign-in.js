26fdb2ef3c7a62a963010765ad79150f9d1325d9
Andrew <46112160+Fybex@users.noreply.github.com>
2021-12-16 14:39:44 +0200

    link path fix

--- src/pages/sign-in.js
+++ src/pages/sign-in.js
@@ -2,8 +2,7 @@ import Avatar from '@mui/material/Avatar'
 import Button from '@mui/material/Button'
 import CssBaseline from '@mui/material/CssBaseline'
 import TextField from '@mui/material/TextField'
-import Link from '@mui/material/Link'
-import Grid from '@mui/material'
+import Grid from '@mui/material/Grid'
 import Box from '@mui/material/Box'
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
 import Typography from '@mui/material/Typography'
@@ -11,6 +10,7 @@ import Container from '@mui/material/Container'
 import { createTheme, ThemeProvider } from '@mui/material/styles'
 import { useCallback } from 'react'
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
+import { Link } from 'react-router-dom'
 
 function Copyright(props) {
   return (
